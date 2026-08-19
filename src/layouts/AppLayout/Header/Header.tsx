67f32cd94d600a234a4e32ea41e12736c2742fd9
akmalzidani <akmal20031003@gmail.com>
2026-08-19 20:58:52 +0800

    refactor: simplify header layout

--- src/layouts/AppLayout/Header/Header.tsx
+++ src/layouts/AppLayout/Header/Header.tsx
@@ -1,30 +1,23 @@
-import { usePageTitle } from "@/hooks/usePageTitle";
 import { useSidebarStore } from "@/stores/useSidebarStore";
 import { useThemeStore } from "@/stores/useThemeStore";
-import { HeaderProfileDropdown } from "./HeaderProfileDropdown";
 import { BsList } from "react-icons/bs";
+import { HeaderProfileDropdown } from "./HeaderProfileDropdown";
 
 function Header() {
-  const currentTitle = usePageTitle();
   const toggleSidebar = useSidebarStore((state) => state.toggleSidebar);
   const theme = useThemeStore((state) => state.theme);
 
   return (
     <header className="sticky-top z-1 px-4 py-3 bg-body border-bottom d-flex align-items-center justify-content-between">
-      <div className="d-flex align-items-center gap-3">
-        <button
-          type="button"
-          className={`btn hover-bg-light border-0 p-1 px-2 ${theme === "dark" ? "text-white" : "text-dark"}`}
-          onClick={toggleSidebar}
-          aria-label="Toggle Sidebar"
-        >
-          <BsList className="fs-4" />
-        </button>
-        <h5 className="mb-0 fw-bold">{currentTitle}</h5>
-      </div>
-      <div className="d-flex align-items-center gap-3">
-        <HeaderProfileDropdown />
-      </div>
+      <button
+        type="button"
+        className={`btn hover-bg-light border-0 p-1 px-2 ${theme === "dark" ? "text-white" : "text-dark"}`}
+        onClick={toggleSidebar}
+        aria-label="Toggle sidebar"
+      >
+        <BsList className="fs-4" />
+      </button>
+      <HeaderProfileDropdown />
     </header>
   );
 }
